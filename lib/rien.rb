6d31c948b6e8a6b0a0ed9bf70ec17b0e7cc12361
Kowalski Dark <darkkowalski2012@gmail.com>
2020-07-30 02:41:03 +0800

    Implement Rienfile and improve CLI tool

--- lib/rien.rb
+++ lib/rien.rb
@@ -1,7 +1,13 @@
 require 'zlib'
 
+require_relative 'rien/core_ext/string'
+
 require_relative 'rien/version'
 require_relative 'rien/const'
 
+require_relative 'rien/configurable'
+
 require_relative 'rien/decoder'
 require_relative 'rien/encoder'
+
+require_relative 'rien/cli'
